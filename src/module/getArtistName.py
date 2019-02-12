bba327d7bad95cf6d8268bdbb15e292158ffced1
xiaozisoft <xiaozisoft@hotmail.com>
2019-02-12 18:16:05 +0800

    get the artist list, add the list

--- src/module/getArtistName.py
+++ src/module/getArtistName.py
@@ -1,8 +1,9 @@
+# code
 from bs4 import BeautifulSoup
 import requests
 import time
-import os
-
+import json
+import sys
 # 头部信息 #需根据自己浏览器的信息进行替换
 headers = {
     'Referer': 'http://music.163.com/',
@@ -12,37 +13,56 @@ headers = {
 }
 
 
+def record_info(filename, info):
+    with open(filename,  'a', encoding='utf-8') as openfile:
+        json.dump(info, openfile, ensure_ascii=False)
+        openfile.write('\n')
+
+
+def get_artist_songs(filename, artistname):
+    with open(filename, 'r', encoding='utf-8') as openfile:
+        for line in openfile:
+            load_dic = json.loads(line)
+            if artistname in load_dic:
+                print(load_dic[artistname])
+                return load_dic[artistname]
+
+
 # 获取歌手信息
 def get_artist_info(artist_id):
     artist_info = dict()
     play_url = 'https://music.163.com/artist?id=' + str(artist_id)
-    print(play_url)
+    # print(play_url)
     s = requests.session()
     s = BeautifulSoup(s.get(play_url, headers=headers).content, 'lxml')
     # print(s)
     songs_info = dict()
     nametext = s.select('h2')
     if len(nametext) != 0:
-        print('歌手：{} 歌手Id：{}'.format(nametext[0].text, nametext[0].attrs['data-rid']))
+        # print('歌手：{} 歌手Id：{}'.format(nametext[0].text, nametext[0].attrs['data-rid']))
         main = s.select('ul.f-hide li a')
         for music in main:
             # print(type(music))
             song_id = music['href'][music['href'].find('id=') + len('id='):]
             songs_info.update({music.text: song_id})
             # print('歌曲名称：{},歌曲Id：{}'.format(music.text, song_id))
-        artist_info.update({artist_id: {nametext[0].text: songs_info}})
-    return songs_info
+        artist_info.update({nametext[0].text: {artist_id: songs_info}})
+    return artist_info
 
 
 if __name__ == "__main__":
     print('开始下载歌曲...\n================================================')
 
     start_time = time.time()  # 开始时间
-
-    for artist_id in range(2000, 2001):
-        test = get_artist_info(artist_id)
+    storename = "./test.txt"
+    for artistid in range(1900, 20000):
+        test = get_artist_info(artistid)
         if len(test) != 0:
-            print(test)
+            # print(test)
+            # record_info(storename, test)
+            record_info(storename, test)
+            #get_artist_songs(storename, 'Andy Kao')
+            #get_artist_songs(storename, 'Andy Kao')
 
     end_time = time.time()  # 结束时间
     print("程序耗时%f秒." % (end_time - start_time))
