d36d334879db04a74de935bb523aa43fc0969172
xiaozisoft <xiaozisoft@hotmail.com>
2019-03-05 18:06:53 +0800

    add example code of reading xlsx

---
+++ src/module/codegenerate.py
@@ -0,0 +1,30 @@
+import xlrd
+
+
+def read_excel(filename, row, column):
+    print(filename, row, column)
+
+    wb = xlrd.open_workbook(filename)  # 打开文件
+
+    print(wb.sheet_names())  # 获取所有表格名字
+
+    #sheet1 = wb.sheet_by_index(1)  # 通过索引获取表格
+
+    sheet = wb.sheet_by_name("IRS List")  # 通过名字获取表格
+
+    print(sheet)
+
+    print(sheet.name, sheet.nrows, sheet.ncols)
+    rows = sheet.row_values(1)  # 获取行内容
+    #cols = sheet.col_values(3)  # 获取列内容
+
+    print(rows)
+
+    print(sheet.cell(1, 0).value)  # 获取表格里的内容，三种方式
+    print(sheet.cell_value(1, 0))
+    print(sheet.row(1)[0].value)
+
+
+if __name__ == "__main__":
+    print("start generate code")
+    read_excel("test.xlsx", 1, 1)
